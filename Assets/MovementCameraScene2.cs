861e8f18710bf006b015f9fe71e1e8ef3c51e59b
mirzalorena <mlie2568@scs.ubbcluj.ro>
2021-01-10 16:28:47 +0200

    scene 2 without door handle

---
+++ Assets/MovementCameraScene2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCameraScene2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float thrust = 2.0f;
+
+    void FixedUpdate()
+    {
+        if (transform.position.z > 1.2)
+        {
+            transform.position += this.transform.forward * thrust * Time.deltaTime * -1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
