2a172de2523b60b5c7e28ad47ac4350a5a1c2796
mocamanuel <mocaemanuel@yahoo.com>
2021-01-10 13:43:39 +0200

    Started Scene 3

---
+++ Assets/MoveCameraScene1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraScene1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float thrust = 2.0f;
+
+    void FixedUpdate()
+    {
+        if (transform.position.z > 1.9)
+        {
+            transform.position += this.transform.forward * thrust * Time.deltaTime;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
